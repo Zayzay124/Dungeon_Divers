aa504a2480daccb991fff4599d06ad8d2516a396
Isaiah Rettig <isaiah.rettig@icloud.com>
2024-09-18 01:27:06 -0400

    Stuff

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 ##Signals
-#taken_damage # talks to HUD
+signal taken_damage # talks to HUD
 #picked up tiem
 
 @export var speed:int = 300
@@ -9,6 +9,8 @@ extends CharacterBody2D
 @export var magic_points:int = 50
 
 var input_dir:Vector2 = Vector2.ZERO
+
+var is_invincible:bool = false
 
 ##PreLoad Scenes
 #load melee attack scene
@@ -20,6 +22,9 @@ func _ready():
 	#add them as children
 	pass
 
+func _process(delta):
+	pass
+
 func _physics_process(delta):
 	input_dir = Input.get_vector("left","right","up","down");
 	velocity = input_dir * speed
@@ -27,10 +32,28 @@ func _physics_process(delta):
 	move_and_collide(velocity * delta)
 
 func dash():
+	#velocity += Vector2(brenden's answer)
+	get_tree().create_timer(.75)
 	pass
 
 func weapon_attack():
 	pass
 
 func magic_attack():
+	pass
+
+func hit(amount):
+	print("taken_damage")
+	taken_damage.emit()
+	#modify health
+
+##Collision check
+#when invincible is true
+
+
+
+func _on_hurtbox_body_entered(body):
+	if body.is_in_group("enemy") and not is_invincible:
+		#hit(body.get_damage())
+		pass
 	pass
