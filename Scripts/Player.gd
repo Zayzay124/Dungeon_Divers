eb6dee5f903fb50f95bf35caa4f9faa2c5ecf93d
Isaiah Rettig <isaiah.rettig@icloud.com>
2024-09-21 17:25:21 -0400

    Dash Completed
    Rudimentary dash implemented

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,13 +2,18 @@ extends CharacterBody2D
 
 ##Signals
 signal taken_damage # talks to HUD
-#picked up tiem
+#picked up time
 
 @export var speed:int = 300
 @export var health:int = 0
 @export var magic_points:int = 50
+@export var dash_speed:int = 10
+
 
 var input_dir:Vector2 = Vector2.ZERO
+
+var dash_dir:Vector2 = Vector2.ZERO
+var can_dash:bool = true
 
 var is_invincible:bool = false
 
@@ -27,14 +32,29 @@ func _process(delta):
 
 func _physics_process(delta):
 	input_dir = Input.get_vector("left","right","up","down");
-	velocity = input_dir * speed
 	
+	if Input.is_action_pressed("right") || Input.is_action_pressed("left"):
+		input_dir.y = 0
+	elif Input.is_action_pressed("up") || Input.is_action_pressed("down"):
+		input_dir.x = 0
+	else:
+		input_dir = Vector2.ZERO
+	
+	input_dir = input_dir.normalized()
+	
+	velocity = input_dir * speed
 	move_and_collide(velocity * delta)
 
+func _input(event):
+	if event.is_action_pressed("dash") and can_dash:
+		dash()
+
 func dash():
-	#velocity += Vector2(brenden's answer)
-	get_tree().create_timer(.75)
-	pass
+	speed += 300
+	can_dash = false
+	is_invincible = true
+	velocity = input_dir * dash_speed
+	$DashTimer.start()
 
 func weapon_attack():
 	pass
@@ -57,3 +77,9 @@ func _on_hurtbox_body_entered(body):
 		#hit(body.get_damage())
 		pass
 	pass
+
+
+func _on_dash_timer_timeout():
+	speed = 300
+	can_dash = true
+	is_invincible = false
