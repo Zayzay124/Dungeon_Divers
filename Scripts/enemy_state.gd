156452c89173ca4debb7d26d29ffa7ec98f11e6f
Isaiah Rettig <isaiah.rettig@icloud.com>
2024-09-30 11:49:18 -0400

    State Machine Classes
    - state machine script that can be attached to any enemy
    - enemy state class that is used in above script
    - blob tree hierarchy

---
+++ Scripts/enemy_state.gd
@@ -0,0 +1,25 @@
+extends Node
+class_name EnemyState
+
+signal transitioned
+
+@export var enemy: CharacterBody2D
+@export var move_speed := 100.0
+
+@onready var player: CharacterBody2D = get_tree().get_first_node_in_group("player")
+@onready var anim_player = get_parent().get_parent().get_node("AnimationPlayer")
+
+var player_direction: Vector2
+
+
+func enter():
+	pass
+
+func exit():
+	pass
+
+func update(_delta):
+	pass
+
+func physics_update(_delta):
+	pass
