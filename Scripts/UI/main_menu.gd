c81977ef78f2bd7e62b4cefaee4f416e1b1e2eae
amazurr <alyssa.mazur1@marist.edu>
2024-12-03 23:23:17 -0500

    Main Menu UI Changes
    - Added more button sprites for when it was in different states (i.e., when was normal, hover, pressed/clicked, etc.).
    - Reworked popups when clicking "Tutorial" and "Credits" button and added our page sprite and necessary text for each respective popup.
    - Made a quick "Exit" button and reworked the functionality.
    - All buttons and popups have functionality/logic to them and work as intended.

--- Scripts/UI/main_menu.gd
+++ Scripts/UI/main_menu.gd
@@ -2,10 +2,11 @@ extends Control
 
 # Reference to the Tutorial and Credits popups
 @onready var tutorial_popup = $VBoxContainer/TutorialButton/TutorialPopup
-@onready var options_popup = $VBoxContainer/OptionsButton/OptionsPopup
+@onready var credits_popup = $VBoxContainer/CreditsButton/CreditsPopup
 
 func _ready() -> void:
-	pass
+	tutorial_popup.visible = false
+	credits_popup.visible = false
 
 func _process(delta: float) -> void:
 	pass
@@ -14,16 +15,16 @@ func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Levels/level.tscn")
 
 func _on_tutorial_button_pressed() -> void:
-	tutorial_popup.popup_centered()  # Shows the tutorial popup in the center
+	tutorial_popup.visible = true  # Shows the tutorial popup
 
 func _on_tutorial_exit_button_pressed() -> void:
-	tutorial_popup.hide()
+	tutorial_popup.visible = false
 	
-func _on_options_button_pressed() -> void:
-	options_popup.popup_centered() # Shows the options popup in the center
+func _on_credits_button_pressed() -> void:
+	credits_popup.visible = true # Shows the options popup
 	
-func _on_options_exit_button_pressed() -> void:
-	options_popup.hide()
+func _on_credits_exit_button_pressed() -> void:
+	credits_popup.visible = false
 	
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
