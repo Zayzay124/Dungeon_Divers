ca856ab3af674c7f93830abe02cfe60a6a2bb0fa
Isaiah Rettig <isaiah.rettig@icloud.com>
2024-12-10 22:19:27 -0500

    Goal Object added
    - when player touches goal they are sent to main menu

--- Scripts/goal.gd
+++ Scripts/goal.gd
@@ -5,4 +5,4 @@ extends Area2D
 
 
 func _on_area_entered(area):
-	pass # get_tree().change_scene_to_file("") go to win screen
+	get_tree().change_scene_to_file("res://Scenes/Levels/main_menu.tscn") #go to main menu screen
